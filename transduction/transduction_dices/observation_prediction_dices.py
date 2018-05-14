75020ee4684ea312cb12f4d2b581c8597bdc570a
josd <josderoo@gmail.com>
2018-05-15 00:45:31 +0200

    refresh

--- transduction/transduction_dices/observation_prediction_dices.py
+++ transduction/transduction_dices/observation_prediction_dices.py
@@ -9,7 +9,7 @@ class ObservationPredictionDices(text_problems.Text2TextProblem):
 
   @property
   def approx_vocab_size(self):
-    return 2**8  # ~256
+    return 2**14  # ~16k
 
   @property
   def is_generate_per_split(self):
@@ -36,7 +36,7 @@ class ObservationPredictionDices(text_problems.Text2TextProblem):
       outcome = random.randint(1, 6)
       yield {
         "inputs": "A_THROW " + repr(n),
-        "targets": repr(outcome)
+        "targets": " " + repr(outcome)
       }
 
 if __name__ == '__main__':
