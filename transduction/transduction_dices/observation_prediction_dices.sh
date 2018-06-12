33372012486805cbfd70b3df331ab15d2b95b02b
josd <josderoo@gmail.com>
2018-06-13 00:06:18 +0200

    retrain

--- transduction/transduction_dices/observation_prediction_dices.sh
+++ transduction/transduction_dices/observation_prediction_dices.sh
@@ -17,7 +17,7 @@ t2t-datagen \
   --t2t_usr_dir=$USER_DIR \
   --tmp_dir=$TMP_DIR
 
-# Train
+# Train with Adam for 700 steps
 t2t-trainer \
   --data_dir=$DATA_DIR \
   --eval_steps=10 \
@@ -27,18 +27,29 @@ t2t-trainer \
   --output_dir=$TRAIN_DIR \
   --problem=$PROBLEM \
   --t2t_usr_dir=$USER_DIR \
-  --train_steps=1000 \
-  --worker_gpu_memory_fraction=0.75
+  --train_steps=700
 
-# Decode
-t2t-decoder \
+# Train with SGD for 700 steps
+t2t-trainer \
   --data_dir=$DATA_DIR \
-  --decode_from_file=sample_dices.observation \
-  --decode_hparams="beam_size=6,alpha=0.6,return_beams=True,write_beam_scores=True" \
-  --decode_to_file=sample_dices.prediction \
+  --eval_steps=10 \
+  --hparams="optimizer=SGD" \
   --hparams_set=$HPARAMS \
+  --local_eval_frequency=100 \
   --model=$MODEL \
   --output_dir=$TRAIN_DIR \
   --problem=$PROBLEM \
   --t2t_usr_dir=$USER_DIR \
-  --worker_gpu_memory_fraction=0.75
+  --train_steps=1400
+
+# Decode
+t2t-decoder \
+  --data_dir=$DATA_DIR \
+  --decode_from_file=train_dices.observation \
+  --decode_hparams="beam_size=6,alpha=0.6,return_beams=True,write_beam_scores=True" \
+  --decode_to_file=train_dices.prediction \
+  --hparams_set=$HPARAMS \
+  --model=$MODEL \
+  --output_dir=$TRAIN_DIR \
+  --problem=$PROBLEM \
+  --t2t_usr_dir=$USER_DIR
