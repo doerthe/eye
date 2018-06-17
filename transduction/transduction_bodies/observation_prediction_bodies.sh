7433536c00d7e4482e642ec8f759487e208f41a8
josd <josderoo@gmail.com>
2018-06-17 13:50:01 +0200

    extended training with SGD optimizer

--- transduction/transduction_bodies/observation_prediction_bodies.sh
+++ transduction/transduction_bodies/observation_prediction_bodies.sh
@@ -17,7 +17,7 @@ t2t-datagen \
   --t2t_usr_dir=$USER_DIR \
   --tmp_dir=$TMP_DIR
 
-# Train
+# Train with Adam for 2000 steps
 t2t-trainer \
   --data_dir=$DATA_DIR \
   --eval_steps=10 \
@@ -27,18 +27,29 @@ t2t-trainer \
   --output_dir=$TRAIN_DIR \
   --problem=$PROBLEM \
   --t2t_usr_dir=$USER_DIR \
-  --train_steps=2000 \
-  --worker_gpu_memory_fraction=0.75
+  --train_steps=2000
+
+# Train with SGD for 1000 steps
+t2t-trainer \
+  --data_dir=$DATA_DIR \
+  --eval_steps=10 \
+  --hparams="optimizer=SGD" \
+  --hparams_set=$HPARAMS \
+  --local_eval_frequency=100 \
+  --model=$MODEL \
+  --output_dir=$TRAIN_DIR \
+  --problem=$PROBLEM \
+  --t2t_usr_dir=$USER_DIR \
+  --train_steps=3000
 
 # Decode
 t2t-decoder \
   --data_dir=$DATA_DIR \
   --decode_from_file=sample_bodies.observation \
-  --decode_hparams="beam_size=3,alpha=0.6,return_beams=True,write_beam_scores=True" \
+  --decode_hparams="beam_size=2,alpha=0.6,return_beams=True,write_beam_scores=True" \
   --decode_to_file=sample_bodies.prediction \
   --hparams_set=$HPARAMS \
   --model=$MODEL \
   --output_dir=$TRAIN_DIR \
   --problem=$PROBLEM \
-  --t2t_usr_dir=$USER_DIR \
-  --worker_gpu_memory_fraction=0.75
+  --t2t_usr_dir=$USER_DIR
