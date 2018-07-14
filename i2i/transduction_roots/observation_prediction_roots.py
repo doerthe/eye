703d37d8e39f38fe4e09069986e8b428c282e6f7
josd <josderoo@gmail.com>
2018-07-14 12:12:21 +0200

    added

---
+++ i2i/transduction_roots/observation_prediction_roots.py
@@ -0,0 +1,56 @@
+import random
+import cmath
+from tensor2tensor.data_generators import problem
+from tensor2tensor.data_generators import text_problems
+from tensor2tensor.utils import registry
+
+@registry.register_problem
+class ObservationPredictionRoots(text_problems.Text2TextProblem):
+  """Transduction from observation to prediction for roots of quadratic equation ax**2 + bx + c = 0"""
+
+  @property
+  def approx_vocab_size(self):
+    return 2**14  # ~16k
+
+  @property
+  def is_generate_per_split(self):
+    # generate_data will shard the data into TRAIN and EVAL for us.
+    return False
+
+  @property
+  def dataset_splits(self):
+    """Splits of data to produce and number of output shards for each."""
+    return [{
+        "split": problem.DatasetSplit.TRAIN,
+        "shards": 7,
+    }, {
+        "split": problem.DatasetSplit.EVAL,
+        "shards": 3,
+    }]
+
+  def generate_samples(self, data_dir, tmp_dir, dataset_split):
+    del data_dir
+    del tmp_dir
+    del dataset_split
+
+    for n in range(100000):
+      # coefficient a
+      a = 1
+      # coefficient b
+      b = random.randint(-50, +50)
+      # coefficient c
+      c = random.randint(-50, +50)
+      # roots
+      r1 = (-b-cmath.sqrt(b**2-4*a*c))/(2*a)
+      r2 = (-b+cmath.sqrt(b**2-4*a*c))/(2*a)
+      # feed the protocol buffer
+      yield {
+        "inputs": "A_POLYNOMIAL with coefficients " + repr(a) + " " + repr(b) + " " + repr(c),
+        "targets": "A_ROOT real " + r1.real.__format__('.2f') + " imag " + r1.imag.__format__('.2f') + \
+          " A_ROOT real " + r2.real.__format__('.2f') + " imag " + r2.imag.__format__('.2f')
+      }
+
+if __name__ == '__main__':
+  gen = ObservationPredictionRoots.generate_samples(None, None, None, None)
+  for i in gen:
+    print(i)
