19d150dc4093ef53f7a0cf3fd49b376b78e1ae0e
josd <josderoo@gmail.com>
2018-08-10 00:34:03 +0200

    retest

--- i2i/dices/run.sh
+++ i2i/dices/run.sh
@@ -46,14 +46,5 @@ t2t-trainer \
   --t2t_usr_dir=$USER_DIR \
   --train_steps=7500
 
-# decode
-t2t-decoder \
-  --data_dir=$DATA_DIR \
-  --decode_from_file=test_dices.observation \
-  --decode_hparams="beam_size=2,alpha=0.6,return_beams=True,write_beam_scores=True" \
-  --decode_to_file=test_dices.prediction \
-  --hparams_set=$HPARAMS \
-  --model=$MODEL \
-  --output_dir=$TRAIN_DIR \
-  --problem=$PROBLEM \
-  --t2t_usr_dir=$USER_DIR
+# test the dices model
+./test.sh
---
+++ i2i/dices/test.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+PROBLEM=observation_prediction_dices
+MODEL=transformer
+HPARAMS=transformer_small
+
+USER_DIR=$PWD
+DATA_DIR=/tmp/t2t_data/$PROBLEM
+TMP_DIR=/tmp/t2t_datagen/$PROBLEM
+TRAIN_DIR=/tmp/t2t_train/$PROBLEM/$MODEL-$HPARAMS
+
+t2t-decoder \
+  --data_dir=$DATA_DIR \
+  --decode_from_file=test_dices.observation \
+  --decode_hparams="beam_size=6,alpha=0.6,return_beams=True,write_beam_scores=True" \
+  --decode_to_file=test_dices.prediction \
+  --hparams_set=$HPARAMS \
+  --model=$MODEL \
+  --output_dir=$TRAIN_DIR \
+  --problem=$PROBLEM \
+  --t2t_usr_dir=$USER_DIR
